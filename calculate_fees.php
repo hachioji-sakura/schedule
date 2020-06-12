045e48fa5efe574f59ac06b3b73f3e932851695c
ToshiakiKatsu <cwkatsucom@yahoo.co.jp>
2020-06-12 12:05:12 +0900

    季節講習DB不一致対応

--- calculate_fees.php
+++ calculate_fees.php
@@ -685,7 +685,7 @@ try{
 					}
 					if (!$item) {
 						if (strpos($event_item['cal_evt_summary'],'季節講習演習')===false) {
-							errMsgFileLog ("季節講習データ不一致<BR>".
+							errMsgFileLog ("注意：季節講習データ不一致<BR>".
 								"事務DB：{$item1['date']} {$item1['stime']},{$item1['teacher_no']},{$item1['$member_no']}<br>".
 								"SON：$date0 $time0 {$event_item['cal_evt_summary']}");
 //							continue;
