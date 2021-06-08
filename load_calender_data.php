00504e94f294fed7d3836fc4c693c5f906c55aff
ToshiakiKatsu <cwkatsucom@yahoo.co.jp>
2021-06-08 13:09:13 +0900

    立川校追加

--- load_calender_data.php
+++ load_calender_data.php
@@ -579,6 +579,9 @@ if ($row[id]==29344) continue;	//2020/11/20 安田さん削除エラー対応
 		case '12':		// 吉祥寺
 			$new_place_id = 13 ;
 			break;
+		case '13':		// 立川
+			$new_place_id = 14 ;
+			break;
 		}
 
 		if ($user_id > 200000 ) {	// スタッフの場合
